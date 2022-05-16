ad7b6015ca896a36256bad1f8d3267441b3272e3
c0stra <ondrej.fischer@gmail.com>
2022-05-16 15:13:12 +0200

    Utilize the ES6 rest parameter for attribute builders.

--- src/test/javascript/test.js
+++
@@ -1,9 +0,0 @@
-
-var model = newToggleModel()
-var enabled = newToggleModel()
-
-body().add(
-    div().title(X('a ', mapToggleModel(model, 'X', 'Y'), ' x')).add('Click').onClick(toggle(model)),
-    expander(model).color(mapToggleModel(enabled, null, 'silver')),
-    div().display(model).add('Hidden element')
-)
