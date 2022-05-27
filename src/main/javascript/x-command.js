af78b5d8cc700481dcc28c8f4675147f83f64026
c0stra <ondrej.fischer@gmail.com>
2022-05-27 12:45:04 +0200

    Update x-command.js

--- src/main/javascript/x-command.js
+++ src/main/javascript/x-command.js
@@ -6,3 +6,20 @@ function toggle(model) {
 function set(model, value) {
     return () => model.set(value)
 }
+
+function copyToClipboard(node) {
+    return function(event) {
+        if(document.selection) {
+            let ieRange = document.body.createTextRange()
+            ieRange.moveToElementText(node)
+            ieRange.select().createTextRange()
+        } else if(window.getSelection) {
+            let domRange = document.createRange()
+            domRange.selectNode(node)
+            window.getSelection().removeAllRanges()
+            window.getSelection().addRange(domRange)
+            document.execCommand("copy")
+            window.getSelection().removeAllRanges()
+        }
+    }
+}
